c299678dc5307c6ded88a1167084925037454510
Sirius Wu <ccwu660601@gmail.com>
2025-02-18 23:23:46 +0800

    Run c_server for 6 seconds

--- ws-server/c_examples/c_server.c
+++ ws-server/c_examples/c_server.c
@@ -4,27 +4,27 @@
 
 struct Server
 {
-    struct WSServer * ws;
+    struct WSServer *ws;
     int opened;
 };
 
-void on_open_cb (void * pointer, const char * src)
+void on_open_cb(void *pointer, const char *src)
 {
-    struct Server * server = (struct Server *) pointer;
+    struct Server *server = (struct Server *)pointer;
     server->opened = 1;
     printf("WS Open: %s\n", src);
 }
 
-void on_error_cb (void * pointer, const char * src)
+void on_error_cb(void *pointer, const char *src)
 {
-    struct Server * server = (struct Server *) pointer;
+    struct Server *server = (struct Server *)pointer;
     server->opened = 0;
     printf("WS client error: %s\n", src);
 }
 
-void on_message_cb (void * pointer, const char * src)
+void on_message_cb(void *pointer, const char *src)
 {
-    struct Server * server = (struct Server *) pointer;
+    struct Server *server = (struct Server *)pointer;
     printf("on_message: %s\n", src);
 
     // 回傳相同的訊息給 client
@@ -35,25 +35,24 @@ int main()
 {
 
     struct Server server;
-    server.opened =0;
+    server.opened = 0;
 
     // New WS server
     server.ws = server_new(10, 3013);
     // Set on_open callback
-    server_set_on_open_cb(server.ws, (void *) &server, on_open_cb);
+    server_set_on_open_cb(server.ws, (void *)&server, on_open_cb);
     // Set on_error callback
-    server_set_on_error_cb(server.ws, (void *) &server, on_error_cb);
+    server_set_on_error_cb(server.ws, (void *)&server, on_error_cb);
     // Set on_message callback
-    server_set_on_message_cb(server.ws, (void *) &server, on_message_cb);
+    server_set_on_message_cb(server.ws, (void *)&server, on_message_cb);
 
     // Server listen
-    printf("Server Listen: %d\n",server_listen(server.ws));
+    printf("Server Listen: %d\n", server_listen(server.ws));
 
-
-    while (1)
+    printf("Run server for 6 seconds\n");
+    while (6)
     {
         sleep(1);
     }
     return 0;
-
 }
